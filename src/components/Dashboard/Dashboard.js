58a8747a8fb4feb634961b9564570887e3539d0e
yeffry <yeffry45@gmail.com>
2022-09-09 22:06:17 -0400

    add basic layout JSX for component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,11 +2,51 @@ import React, { useState } from "react";
 import styles from "../Dashboard/Dashboard.module.scss";
 
 const Dashboard = () => {
-  const [open, setOpen] = useState(true);
-
   return (
     <section className={styles.dashboard}>
       <p>Get started working on your projects!</p>
+
+      <div className={styles["grid-container"]}>
+        <div className={styles["grid-item"]}>
+          <h4>In Progress</h4>
+          <span>60</span>
+        </div>
+
+        <div className={styles["grid-item"]}>
+          <h4>In Progress</h4>
+          <span>60</span>
+        </div>
+
+        <div className={styles["grid-item"]}>
+          <h4>In Progress</h4>
+          <span>60</span>
+        </div>
+
+        <div className={styles["grid-item"]}>
+          <h4>In Progress</h4>
+          <span>60</span>
+        </div>
+      </div>
+
+      <div className={styles["flex-container"]}>
+        <div className={styles["flex-item"]}>
+          <h4>Projects</h4>
+          <ul>
+            <li>Todo List</li>
+            <li>Issue Tracker</li>
+            <li>Weather App</li>
+          </ul>
+        </div>
+
+        <div className={styles["flex-item"]}>
+          <h4>Issues</h4>
+          <ul>
+            <li>Todo List</li>
+            <li>Issue Tracker</li>
+            <li>Weather App</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
