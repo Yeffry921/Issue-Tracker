ceac386430528d172149a1cbf7dc45c9d3c7c755
yeffry <yeffry45@gmail.com>
2022-09-13 23:29:33 -0400

    fix layout issues + minor routing issues

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Navbar from "./components/Navbar/Navbar";
 import Dashboard from "./components/Dashboard/Dashboard";
 import "./App.css";
 import Sidebar from "./components/UI/Sidebar";
+import Projects from "./components/Projects/Projects";
+import { Routes, Route } from "react-router-dom";
+import Issues from "./components/Issues/Issues";
+import BugTable from "./components/UI/BugTable";
 
 const App = () => {
   return (
-    <div>
+    <div className="layout-container">
       <Navbar />
-      <main style={{ display: "flex" }}>
+      <main>
         <Sidebar />
-        <Dashboard />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/issues" element={<BugTable />} />
+        </Routes>
       </main>
     </div>
   );
