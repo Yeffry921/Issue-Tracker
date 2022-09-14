3364e93e9766c9097151271bcf3cc5af22443212
yeffry <yeffry45@gmail.com>
2022-09-14 13:13:06 -0400

    add project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,35 +1,14 @@
 import React from "react";
-import Table from "../UI/Table";
 import styles from "../Projects/Projects.module.scss";
+import ProjectTable from "../UI/ProjectTable";
 
 const Projects = () => {
-  const mockDProjects = [
-    {
-      name: "Sample Project #1",
-      id: 1,
-    },
-    {
-      name: "Sample Project #2",
-      id: 2,
-    },
-    {
-      name: "Sample Project #3",
-      id: 3,
-    },
-  ];
-  return <section className={styles.projects}>
-    {mockDProjects.map((projects) => (
-      <div
-        key={projects.id}
-        style={{
-          margin: "25px",
-          cursor: "pointer",
-        }}
-      >
-        {projects.name}
-      </div>
-    ))}
-  </section>;
+  return (
+    <section className={styles.projects}>
+      <h1>Projects</h1>
+      <ProjectTable />
+    </section>
+  );
 };
 
 export default Projects;
