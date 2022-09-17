8e2c808cc328387a46846105bcaff22a8279cd08
yeffry <yeffry45@gmail.com>
2022-09-17 12:28:10 -0400

    changed sidebar location

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ import "./App.css";
 import Sidebar from "./components/UI/Sidebar";
 import Projects from "./components/Projects/Projects";
 import { Routes, Route } from "react-router-dom";
-import Issues from './components/Issues/Issues'
+import Issues from "./components/Issues/Issues";
 
 const App = () => {
   return (
     <div className="layout-container">
       <Navbar />
       <main>
-        <Sidebar />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/issues" element={<Issues/>} />
         </Routes>
       </main>
     </div>
