efbde3aee7edb7ad0a72620cda78b69e56ac17a7
yeffry <yeffry45@gmail.com>
2022-09-20 11:00:40 -0400

    move to its own directory

---
+++ src/components/UI/BugTable/BugTable.js
@@ -0,0 +1,83 @@
+import React from "react";
+import styles from "../BugTable/BugTable.module.scss"
+
+const BugTable = () => {
+  return (
+    <div className={styles.container}>
+      <button>Submit Issue</button>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Bug</th>
+            <th>Created</th>
+            <th>Reporter</th>
+            <th>Due</th>
+            <th>Status</th>
+            <th>Severity</th>
+          </tr>
+        </thead>
+
+        <tbody className={styles["table-body"]}>
+          <tr>
+            <td className={styles.flex}>
+              <input type="checkbox" />
+              <a href="#">Project A Form is broken</a>
+            </td>
+
+            <td>09/21/2022</td>
+
+            <td className={styles.reporter}>
+              <span>Antonio Yellow</span>
+            </td>
+
+            <td>06/10/2023</td>
+            <td className={styles.status}>
+              <span>In Progress</span>
+            </td>
+            <td>Major</td>
+          </tr>
+
+          <tr>
+            <td>
+              <input type="checkbox" />
+              <a href="#">Project A Form is broken</a>
+            </td>
+
+            <td>09/21/2022</td>
+
+            <td>
+              <span>Antonio Yellow</span>
+            </td>
+
+            <td>06/10/2023</td>
+            <td className={styles.status}>
+              <span>In Progress</span>
+            </td>
+            <td>Major</td>
+          </tr>
+
+          <tr>
+            <td>
+              <input type="checkbox" />
+              <a href="#">Project A Form is broken</a>
+            </td>
+
+            <td>09/21/2022</td>
+
+            <td>
+              <span>Antonio Yellow</span>
+            </td>
+
+            <td>06/10/2023</td>
+            <td className={styles.status}>
+              <span>In Progress</span>
+            </td>
+            <td>Major</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BugTable;
